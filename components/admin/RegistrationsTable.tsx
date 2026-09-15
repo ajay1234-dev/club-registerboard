53d915e5d06c0baa7f0472c1a1830d677b714908
ajay1234-dev <inderajay82@gmail.com>
2026-09-16 02:11:35 +0530

    first commit

--- components/admin/RegistrationsTable.tsx
+++ components/admin/RegistrationsTable.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, useCallback } from "react";
-import { format } from "date-fns";
+
 import { Search, ChevronLeft, ChevronRight, Download, Filter } from "lucide-react";
 import Button from "@/components/ui/Button";
 import Input from "@/components/ui/Input";
